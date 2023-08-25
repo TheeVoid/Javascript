d8c0768b37b18728da6891d78a7ac9aebbc5380c
TheeVoid <92278175+TheeVoid@users.noreply.github.com>
2023-08-26 01:14:00 +0200

    Test

---
+++ website/test.js
@@ -0,0 +1,8 @@
+
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today  = new Date();
+
+console.log(today.toLocaleDateString("en-US")); // 9/17/2016
+console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
+console.log(today.toLocaleDateString("hi-IN", options)); // शनिवार, 17 सितंबर 2016
